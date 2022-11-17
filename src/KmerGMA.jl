729e6132289f1844af585ede3ed17696d235d0f7
Qile <qile0317@gmail.com>
2022-11-17 02:25:42 +0100

    reconfigured deps

--- src/KmerGMA.jl
+++ src/KmerGMA.jl
@@ -1,10 +1,9 @@
 module KmerGMA
-    using Plots, BioSequences, FASTX, Distances, ProgressMeter, StringViews #WebBlast, Statistics, StringDistances, StatsBase, StatsPlots, DataFrames
+    using BioSequences, FASTX, Distances #WebBlast, Statistics, StringDistances, StatsBase, StatsPlots, DataFrames
     include("simpleExplore.jl")
     include("ExactMatch.jl")
     include("GMAutils.jl")
     include("RefGen.jl")
     include("GMA.jl")
     include("API.jl")
-    #include("BLAST.jl")
 end
