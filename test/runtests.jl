e61e9e4014cb6d921f511c9bebdad1a6a55b5090
Qile <qile0317@gmail.com>
2023-03-25 17:44:27 +0100

    set test_locally back to false

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Test, BioSequences, FASTX, Random, BioAlignments, Distances, StaticArrays
 
 # for devs: if testing the script on your own machine, set the following variable to true. Otherwise must set to false when pushing
-test_locally = true
+test_locally = false
 
 #setting testing variables
 if test_locally
